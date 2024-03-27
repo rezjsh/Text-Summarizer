1beadd7465c81b946a2564b53606c1b679c14e20
rezjsh <149146755+rezjsh@users.noreply.github.com>
2024-03-27 16:19:51 +0330

    data validation functionality

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from textsummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textsummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textsummarizer.logging import logger
 
 
-STAGE_NAME = 'Data Ingestion stage'
+# STAGE_NAME = 'Data Ingestion stage'
+# try:
+#     logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+#     data_ingestion = DataIngestionTrainingPipeline()
+#     data_ingestion.main()
+#     logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx=======x")
+# except Exception as e:
+#     raise e
+
+STAGE_NAME = 'Data Validation stage'
 try:
     logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
-    data_ingestion = DataIngestionTrainingPipeline()
-    data_ingestion.main()
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
     logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx=======x")
 except Exception as e:
     raise e
